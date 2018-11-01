b1baf74e6f4881f6ca52bc096c292a204beb581f
sutichunxiao <15586784538@163.com>
2018-11-01 14:13:51 +0800

    add main.h file

---
+++ main.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: main.h
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2018年11月 1日 14:12:26
+ ************************************************************************/
+#ifndef MAIN_H_
+#define MAIN_H_
+#include<iostream>
+using namespace std;
+#endif
